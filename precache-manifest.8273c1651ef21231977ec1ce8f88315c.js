a4a75181c658c64113da08638923288c69a183bf
harikrishna2208 <harikrishna2278@gmail.com>
2020-08-22 14:05:43 +0530

    Updates

---
+++ precache-manifest.8273c1651ef21231977ec1ce8f88315c.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "635c3a349bf5aac79727a17ae71ac41a",
+    "url": "/react-music-app/index.html"
+  },
+  {
+    "revision": "2ca88c95573404e67508",
+    "url": "/react-music-app/static/css/2.0c0672e5.chunk.css"
+  },
+  {
+    "revision": "58f61ce03192404b709b",
+    "url": "/react-music-app/static/css/main.d4407272.chunk.css"
+  },
+  {
+    "revision": "2ca88c95573404e67508",
+    "url": "/react-music-app/static/js/2.71e45a81.chunk.js"
+  },
+  {
+    "revision": "c97fb91e7dd1c7b19ae67fb152a2e6b2",
+    "url": "/react-music-app/static/js/2.71e45a81.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "58f61ce03192404b709b",
+    "url": "/react-music-app/static/js/main.d65adbaa.chunk.js"
+  },
+  {
+    "revision": "837ae8355ecbe765db00",
+    "url": "/react-music-app/static/js/runtime-main.6cf6d477.js"
+  },
+  {
+    "revision": "e175412e95b9aeba27a71b47937e0fad",
+    "url": "/react-music-app/static/media/creativeminds.e175412e.mp3"
+  },
+  {
+    "revision": "80276ac2e9b80cdde919b3c40b4a79cc",
+    "url": "/react-music-app/static/media/happyrock.80276ac2.mp3"
+  },
+  {
+    "revision": "bf0df2e2bb3e6fee0275f6cc7921069c",
+    "url": "/react-music-app/static/media/littleidea.bf0df2e2.mp3"
+  }
+]);
